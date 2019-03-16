4ceac4d7f206ff4cf616181a67538b350d145f6a
Hamster5295 <hamster5295@163.com>
2019-03-16 12:19:50 +0800

    Code

---
+++ src/org/hamgift/main/Main.java
@@ -0,0 +1,12 @@
+package org.hamgift.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin {
+
+    Logger log = getLogger();
+
+
+}
