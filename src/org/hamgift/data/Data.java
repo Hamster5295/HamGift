609d95abc24aa45a8e2d8376705beb82ca160434
Hamster5295 <hamster5295@163.com>
2019-03-20 22:35:05 +0800

    New class: Data&HGift

---
+++ src/org/hamgift/data/Data.java
@@ -0,0 +1,15 @@
+package org.hamgift.data;
+
+import java.util.HashMap;
+
+public class Data {
+
+    //This class is used to save data from Input or custom gifts.
+
+    public static HashMap<String,HGift> gifts = new HashMap<>();
+
+    public static void loadData(){
+
+    }
+
+}
